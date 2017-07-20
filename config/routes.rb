f1dc7fe887413a4429bb8a07ede83be645ef6e48
AFresnedo <AFresnedo@users.noreply.github.com>
2017-07-19 19:31:08 -0700

    add StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/welcome'
+
+  get 'static_pages/support'
+
   root 'placeholder#welcome'
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get welcome" do
+    get static_pages_welcome_url
+    assert_response :success
+  end
+
+  test "should get support" do
+    get static_pages_support_url
+    assert_response :success
+  end
+
+end
