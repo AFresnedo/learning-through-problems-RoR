e915ddc41044fc798e88a2038758a7a554582cae
AFresnedo <AFresnedo@users.noreply.github.com>
2017-07-22 16:04:59 -0700

    add bcrypt password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
-  attr_accessor :name, :email
+  before_save :downcase_email
+  validates :name, presence: true, length: { maximum: 50 }
+  # TODO refactor regex, hopefully in a private method below
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+  # has_many activity_log_entry, dependent: destroy
+
+  private
+    def downcase_email
+      email.downcase!
+    end
 end
