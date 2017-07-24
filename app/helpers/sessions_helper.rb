f2a9609d586de34c11379155a41ebe69ba235417
AFresnedo <AFresnedo@users.noreply.github.com>
2017-07-24 12:28:29 -0700

    add logout func to temp cookie

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,25 +2,21 @@ module SessionsHelper
 
   # browser-life log in credentials, stored in user browser
   def log_in(user)
-    # trusting RoR session code feature to encrypt, store, and delete sessions
-    # user.id will be encrypted in the browser temp cookie
-    # session should always be nil unless user logged in during current
-    # browser's life
     session[:user_id] = user.id
   end
 
   def logged_in
-    # User.find will throw an exception, so User.find_by used instead
-    if User.find_by(id: session[:user_id])
-      true
-    else
-      false
-    end
+    !current_user.nil?
   end
 
   def current_user
-    # prevents multiple database hits, per controller action, by storing
+    # prevents multiple database hits, per controller action/scope, by storing
     # instance variable
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
