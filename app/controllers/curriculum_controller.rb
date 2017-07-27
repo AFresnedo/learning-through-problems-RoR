b7b72ebca250e1beeb77f467d91506a4167230cb
AFresnedo <AFresnedo@users.noreply.github.com>
2017-07-27 16:23:28 -0700

    begin psuedocode of curriculum controller

--- app/controllers/curriculum_controller.rb
+++ app/controllers/curriculum_controller.rb
@@ -1,2 +1,11 @@
 class CurriculumController < ApplicationController
+  # identify user and fetch latest problem (render view)
+  #   -read global and local graphs as well as user's latest created_at in
+  #   score
+  #   -use what is written in those text files to interpolate a render
+  # finalize problem fetch
+  #   -save 'zero' to new solution object
+  # respond and act on actions taken: answer, hint request
+  #   -add appropriate negative number for hints, add appropriate pos # for ans
+  # TODO determine if can easily add or "subtract" to a int attr
 end
