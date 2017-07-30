2e255213c3f0f51f6fbf0a715d0e0a656a63160f
AFresnedo <AFresnedo@users.noreply.github.com>
2017-07-30 14:05:57 -0700

    update load scripts

--- db/migrate/20170730084854_create_theories.rb
+++ db/migrate/20170730084854_create_theories.rb
@@ -1,6 +1,8 @@
 class CreateTheories < ActiveRecord::Migration[5.1]
   def change
     create_table :theories do |t|
+      t.string :filename
+      t.text :text
 
       t.timestamps
     end
--- db/migrate/20170730084906_create_graphs.rb
+++ db/migrate/20170730084906_create_graphs.rb
@@ -1,6 +1,9 @@
 class CreateGraphs < ActiveRecord::Migration[5.1]
   def change
     create_table :graphs do |t|
+      t.string :context
+      t.string :scoring
+      t.text :progression
 
       t.timestamps
     end
