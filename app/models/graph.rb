688815714f8246870f2780e120bdebcc1a9c23f9
AFresnedo <AFresnedo@users.noreply.github.com>
2017-07-31 23:08:33 -0700

    add notes post v1 migrations

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -1,2 +1,6 @@
 class Graph < ApplicationRecord
+  # TODO pull values from string
+  #   takes 1-2 parameters (problem, context) and returns next problem
+  #   takes 1-2 parameters (problem, context) and returns unlocked theories
+  # put values in string and store, less important
 end
