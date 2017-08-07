4331fdd082f0e713d2c13b5581034d371e023ed9
AFresnedo <AFresnedo@users.noreply.github.com>
2017-08-07 16:00:29 -0700

    write plan for first progression test

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,2 +1,9 @@
 class GraphsController < ApplicationController
+  # for now, just index each context and show batches
+  # i want to simulate the globalgraph through user-choice hyperlinks
+  # after that, i want a portal of ACTUAL user choice with headings seperating
+  # each category (ALGEBRAIC, etc, pretend that a user can't skip a heading
+  # section); inside of that are the links to user-choice folders (liquids,
+  # casetry, etc) and then once they pick one of those links i want it to go
+  # through the progression with succeed/fail linkage
 end
