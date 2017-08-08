a0ad824a21d539875dc3da1bbe8ba0b38c7a3e3d
AFresnedo <AFresnedo@users.noreply.github.com>
2017-08-07 17:20:42 -0700

    create graph progression test actions

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,9 +1,18 @@
 class GraphsController < ApplicationController
-  # for now, just index each context and show batches
-  # i want to simulate the globalgraph through user-choice hyperlinks
-  # after that, i want a portal of ACTUAL user choice with headings seperating
-  # each category (ALGEBRAIC, etc, pretend that a user can't skip a heading
-  # section); inside of that are the links to user-choice folders (liquids,
-  # casetry, etc) and then once they pick one of those links i want it to go
-  # through the progression with succeed/fail linkage
+  def globalgraph
+    # teacher-only choice
+    # seperated by headings (ALGEBRAIC, METHODS, etc)
+  end
+
+  def category
+    # user choice
+    # has the intro-to-category theory (displayed)
+    # heading for folders (liquids, etc)
+    # NOTE this is right before progression logic takes over for users
+  end
+
+  def category_edit
+    # admin page for displaying progression information at localgraph level
+    # doing this before hiding it behind progression logic, for debugging
+  end
 end
