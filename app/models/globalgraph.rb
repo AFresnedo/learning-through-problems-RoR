f31c2f1ee7e16c74b8fe6cf0d13da0c65128c22d
AFresnedo <AFresnedo@users.noreply.github.com>
2017-08-12 12:46:46 -0700

    finish first set of gg model methods

--- app/models/globalgraph.rb
+++ app/models/globalgraph.rb
@@ -1,21 +1,39 @@
 class Globalgraph < ApplicationRecord
 
   # returns starting theory and problem files
+  # OR returns starting theory and "context" name
   def Globalgraph.get_beginning
-    beginningMarks = Globalgraph.where(category_order: 0)
-    # format is type: id; example follows - theory: 5
-    startingFiles = []
-    beginningMarks.each do |mark|
-      if mark.context == 'category_introduction'
-        file_id = Theory.find_by(category: mark.category, context:
-                                 mark.context).id
-        startingFiles << 'theory'
-        startingFiles << file_id
-      # else (it is a directory, so graph needs to return all starting files
-      # inside that category directory; should be 1 file per context)
-        # startingFiles << Graph.where(category: mark.category)
-      end
-    end
-    return startingFiles
+    Globalgraph.get_by_order(0)
   end
+
+  # returns next category theory and starting problem files
+  # OR returns next category theory and context name(s)
+  def Globalgraph.get_next(category)
+    order = Globalgraph.find_by(category: category).category_order
+    nextOrder = order+1
+    Globalgraph.get_by_order(nextOrder)
+  end
+
+  private
+
+    # returns list, format is type: id; example follow - theory: 5
+    def Globalgraph.get_by_order(category_order)
+      marks = Globalgraph.where(category_order: category_order)
+      files = []
+      marks.each do |mark|
+        # if it is a theory file
+        if mark.context == 'category_introduction'
+          file_id = Theory.find_by(category: mark.category, context:
+                                   mark.context).id
+          files << 'theory'
+          files << file_id
+        # else it is a directory
+        else
+          files << 'context'
+          files << mark.context
+        end
+      end
+      return files
+    end
+
 end
--- test/models/globalgraph_test.rb
+++ test/models/globalgraph_test.rb
@@ -2,10 +2,21 @@ require 'test_helper'
 
 class GlobalgraphTest < ActiveSupport::TestCase
   def setup
+    # for all
     load "#{Rails.root}/db/seeds/theory_seed.rb"
     load "#{Rails.root}/db/seeds/globalgraph_seed.rb"
+
+    # for beginning files
     first_theory = Theory.find_by(filename: "./INTRODUCTION/theorytheory.html")
     @ft_id = first_theory.id
+
+    # for next category
+    this_theory = Theory.find_by(filename:
+                                 "./APPLICATIONS/explanationtheory2.html")
+    @tt_id = this_theory.id
+    next_theory = Theory.find_by(filename:
+                                 "./ALGEBRAIC/explanationtheory3.html")
+    @nt_id = next_theory.id
   end
 
   test "theory load" do
@@ -25,5 +36,18 @@ class GlobalgraphTest < ActiveSupport::TestCase
     beginning_list = Globalgraph.get_beginning
     assert_equal "theory", beginning_list[0]
     assert_equal beginning_list[1], @ft_id
+    assert_equal "context", beginning_list[2]
+    assert_equal "example", beginning_list[3]
   end
+
+  test "get next category" do
+    category_order = 2
+    thisCategoryList = Globalgraph.get_by_order(category_order)
+    assert_equal "theory", thisCategoryList[0]
+    assert_equal @tt_id, thisCategoryList[1]
+    nextCategoryList = Globalgraph.get_by_order(category_order+1)
+    assert_equal "theory", nextCategoryList[0]
+    assert_equal @nt_id, nextCategoryList[1]
+  end
+
 end
