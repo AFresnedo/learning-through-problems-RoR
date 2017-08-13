42cdaa1a3187469c7e4b781193313023732aab96
AFresnedo <AFresnedo@users.noreply.github.com>
2017-08-13 00:14:04 -0700

    begin model graph psuedocode

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -1,26 +1,14 @@
 class Graph < ApplicationRecord
-  # NOTE DEPRECATED NOTES, not what graph model represents anymore, here for
-  # ref atm
-  # the Graph model is the interface for getting the next problem/theorem in
-  # the curriculum
-  # TODO get ordered-list from context-appropriate model and return list item
-  #   takes 1-2 parameters (problem, context) and returns next problem
-  #     if that was the last problem of the context, return next problem in
-  #     next context OR iff last problem entirely return "end"
-  # TODO determine if Graph should be used to create context-specific ordered
-  # lists or if that should be done at a lower level (hopefully not lower,
-  # ideally the construction begins here and then finished lower as OOP
-  # appropriate)
 
-  # this model represents a table that has tuples representing ordered lists
-  # from teach context...so this model represents the globalgraph ordered list
-  # gg_id, context_title, localgraph_id
-  #
-  # TODO unlock&display the next "explanationtheory" file once a category has
-  # been finished (after the last context, which are the folders with
-  # graph.txt)
+  # get first file in context
 
-  # NOTE real graph model notes follow
-  # "batch" doesn't control ordering of in-set problems, not fixing atm
+  # get next file in context
+
+  # get makeup in set
+
+  # get number of normal problems in set?
+
+  # NOTE some notion of theory or problem file, some notion of sets, some
+  # notion of makeup problem
 
 end
