85cac3be448fb38f957c1fb92c50188ff5c26bf2
AFresnedo <AFresnedo@users.noreply.github.com>
2017-08-13 02:10:07 -0700

    begin coding model graph next file

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -9,6 +9,29 @@ class Graph < ApplicationRecord
     Graph.get_file(category, context, batch, order)
   end
 
+  # TODO last regular file in batch
+  def Graph.get_next_file(typ, file)
+    last = Graph.find_by(typ: typ, file_id: file)
+    nxt = Graph.find_by(category: last.category,
+                        context: last.context,
+                        batch: last.batch,
+                        order: last.order+1)
+    typ = nxt.typ
+    id = nxt.file_id
+    return [typ, id]
+  end
+
+  private
+    def Graph.get_file(category, context, batch, order)
+      tuple = Graph.find_by(category: category,
+                    context: context,
+                    batch: batch,
+                    order: order)
+      typ = tuple.typ
+      id = tuple.file_id
+      return [typ, id]
+    end
+
   # get next nonmakeup file in set, send flag to move onto next set if done
   # get makeup in set
 
--- test/models/graph_test.rb
+++ test/models/graph_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class GraphTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    # curriculum seed causes long loads
+    load "#{Rails.root}/db/seeds/curriculum_seed.rb"
+    load "#{Rails.root}/db/seeds/theory_seed.rb"
+    load "#{Rails.root}/db/seeds/graph_seed.rb"
+
+    # for first file of introduction category
+    first_file = Theory.find_by(category: 'introduction',
+                                context: 'example',
+                                filename: 'exampletheory1.html')
+    @ff_id = first_file.id
+
+    # for next file
+    last_file = Problem.find_by(category: 'methods',
+                                context: 'numerical',
+                                filename: 'numerical2.html')
+    @lf_id = last_file.id
+    next_file = Problem.find_by(category: 'methods',
+                                context: 'numerical',
+                                filename: 'numerical3.html')
+    @nf_id = next_file.id
+  end
+
+  test "get first file of introduction category" do
+    first_file = Graph.get_first_file('introduction', 'example')
+    assert_equal 'theory', first_file[0]
+    assert_equal @ff_id, first_file[1]
+  end
+
+  test "get next file" do
+    next_file = Graph.get_next_file('prob', @lf_id)
+    assert_equal @nf_id, next_file[1]
+  end
+
 end
