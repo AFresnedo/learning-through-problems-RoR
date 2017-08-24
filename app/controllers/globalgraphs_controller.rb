24ab2b3fb4956a916afdaeab5eb3a802a2a32544
AFresnedo <AFresnedo@users.noreply.github.com>
2017-08-24 14:58:38 -0700

    finish globalgraph basic views

--- app/controllers/globalgraphs_controller.rb
+++ app/controllers/globalgraphs_controller.rb
@@ -1,9 +1,11 @@
 class GlobalgraphsController < ApplicationController
 
   def index
+    @globalgraphs = Globalgraph.all
   end
 
   def show
+    @globalgraphs = Globalgraph.where(curriculum: params[:id])
   end
 
 end
