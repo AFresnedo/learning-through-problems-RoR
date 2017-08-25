21440d2a431775a330374b2b0f385ab426651dc6
AFresnedo <AFresnedo@users.noreply.github.com>
2017-08-24 17:23:16 -0700

    finish basic graph index and show

--- app/controllers/globalgraphs_controller.rb
+++ app/controllers/globalgraphs_controller.rb
@@ -1,11 +1,10 @@
 class GlobalgraphsController < ApplicationController
-
   def index
     @globalgraphs = Globalgraph.all
   end
 
   def show
+    # NOTE :id in form of curriculum name, as a string
     @globalgraphs = Globalgraph.where(curriculum: params[:id])
   end
-
 end
--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,7 +1,11 @@
 class GraphsController < ApplicationController
   def index
+    # each context (graph.txt)
+    @graphs = Graph.all
   end
 
   def show
+    # NOTE :id in form of context name, as a string
+    @graphs = Graph.where(context: params[:id])
   end
 end
