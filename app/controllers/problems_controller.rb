8baa04500c0132f0d3d8414ff2b9e25864542569
AFresnedo <AFresnedo@users.noreply.github.com>
2017-08-25 21:17:03 -0700

    refactor problem show action

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -7,13 +7,6 @@ class ProblemsController < ApplicationController
 
   def show
     @problem = Problem.find(params[:id])
-    @answer = @problem.answer
-    @solutions = @problem.solutions
-    @hints = []
-    @solutions.each do |solution|
-      @hints.push(solution.hints)
-    end
-    @metadata = @problem.metadata
   end
 
   def edit
