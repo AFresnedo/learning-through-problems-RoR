9b2d6d5c81b5e31eb1d4a2eeb1aa0e15f159f09d
AFresnedo <AFresnedo@users.noreply.github.com>
2017-09-01 11:41:54 -0700

    finish first attempt at results page look&feel

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -1,12 +1,36 @@
 // colors user answers green if correct, otherwise red
 function highlightAnswers(expectedAlpha, userAlpha, numAnswers) {
   for (i = 0; i < numAnswers; i++) {
-    var expected = document.getElementById(expectedAlpha+'_'+i).innerHTML;
-    var user = document.getElementById(userAlpha+'_'+i).innerHTML;
+    var expected = document.getElementById(expectedAlpha + '_' + i).innerHTML;
+    var user = document.getElementById(userAlpha + '_' + i).innerHTML;
     if (parseFloat(expected) == parseFloat(user)) {
-      document.getElementById(userAlpha+'_'+i).style.color = 'green';
+      document.getElementById(userAlpha + '_' + i).style.color = 'green';
     } else {
-      document.getElementById(userAlpha+'_'+i).style.color = 'red';
+      document.getElementById(userAlpha + '_' + i).style.color = 'red';
     }
+  }
+}
+
+function cummulative_scores() {
+  // get eleemnt
+  var dropListInput = document.getElementById('cummulative_scores');
+  var scope = dropListInput.options[dropListInput.selectedIndex].text;
+  switch (scope) {
+    case 'Context':
+      document.getElementById('to_enter').innerHTML =
+        document.getElementById('con_scr').innerHTML;
+      break;
+    case 'Category':
+      document.getElementById('to_enter').innerHTML =
+        document.getElementById('cat_scr').innerHTML;
+      break;
+    case 'Curriculum':
+      document.getElementById('to_enter').innerHTML =
+        document.getElementById('cur_scr').innerHTML;
+      break;
+    case 'Everything':
+      document.getElementById('to_enter').innerHTML =
+        document.getElementById('tot_scr').innerHTML;
+      break;
   }
 }
