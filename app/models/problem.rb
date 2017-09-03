11e32db868502be9708d0f72355a96b4203b067e
AFresnedo <AFresnedo@users.noreply.github.com>
2017-09-03 13:47:29 -0700

    include no-lowercase-char-leading-text in problem model

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,9 +1,28 @@
 class Problem < ApplicationRecord
+  include ActionView::Helpers # for strip_tags
   has_one :answer
   has_one :metadata
   has_many :solutions
   validates :filename, :text, :category, :context, :curriculum, presence: true
   validates :filename, uniqueness: true
+  validate :text_cannot_begin_lowercase_letter
 
   default_scope -> { order(filename: :asc)}
+
+  private
+
+    def text_cannot_begin_lowercase_letter
+      if first_nonhtml_letter_lowercase? text
+        errors.add(:text, "cannot begin with a lowercase letter")
+      end
+    end
+
+    def first_nonhtml_letter_lowercase? string
+      # remove html
+      string = strip_tags(string)
+      # remove leading whitespace
+      string = string.strip
+      # returns true if first non-(html/whitespace) char is lower else false
+      /[a-z]/.match(string[0]) ? true : false
+    end
 end
