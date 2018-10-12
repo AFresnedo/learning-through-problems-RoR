234b8250eae85d550a5a8f31e590e2ec6199247f
AFresnedo <AFresnedo@users.noreply.github.com>
2018-10-11 19:14:18 -0700

    finish asyncing request hint functionality

--- app/assets/javascripts/solve.js
+++ app/assets/javascripts/solve.js
@@ -1,18 +1,17 @@
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
   console.log('ajax-routing for solve loading on this page');
 
   $('.hints-button').click(function(e) {
     e.preventDefault();
-    console.log('hint requested for:', $('.button_to').attr('action'));
+    console.log('hint requested for:', $(this).attr('action'));
 
-    // $.ajax({
-      // url: $(this).attr('href'),
-      // method: 'POST'
-    // }).done(function(msg) {
-      // console.log('success msg:', msg);
-      // window.location = '/solve';
-    // }).fail(function(msg) {
-      // console.log('err msg:', msg);
-    // });
+    $.ajax({
+      url: $(this).attr('action'),
+      method: 'POST'
+    }).done(function(msg) {
+      console.log('success msg:', msg);
+    }).fail(function(msg) {
+      console.log('err msg:', msg);
+    });
   });
 });
